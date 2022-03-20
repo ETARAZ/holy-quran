19738d163ff322928328d6f4b91299ebbb85336b
napster <napster@gmail.com>
2022-03-20 15:36:01 +0000

    Fix casing discrepancies.

--- pages/surah/[surah].js
+++ pages/surah/[surah].js
@@ -3,23 +3,23 @@ import Head from "next/head";
 import "react-h5-audio-player/lib/styles.css";
 import Ayah from "../../components/ayah";
 
-function Surah({ surah }) {
+function Surah({ data }) {
   return (
     <div className="w-full">
       <Head>
-        <title>{surah.name}</title>
-        <meta name={surah.name} content={surah.name} />
+        <title>{data.name}</title>
+        <meta name={data.name} content={data.name} />
       </Head>
       <div className="mx-auto max-w-3xl">
-        <h2 className="text-3xl  text-center">{surah.name}</h2>
-        <AudioPlayer className="my-3" src={surah.audio} />
+        <h2 className="text-3xl  text-center">{data.name}</h2>
+        <AudioPlayer className="my-3" src={data.audio} />
       </div>
       <p
         lang="ar"
         className="font-quran text-3xl  sm:text-4xl md:text-5xl text-center"
         style={{ direction: "rtl" }}
       >
-        {surah.ayahs.map((ayah, i) => (
+        {data.ayahs.map((ayah, i) => (
           <Ayah key={i} ayah={ayah.text} number={ayah.numberInSurah} />
         ))}
       </p>
@@ -39,12 +39,12 @@ export const getStaticPaths = async () => {
 };
 
 export async function getStaticProps(context) {
-  const { surah: singleSurah } = context.params;
-  const res = await fetch("http://api.alquran.cloud/v1/surah/" + singleSurah);
-  const { data: surah } = await res.json();
-  console.log(surah);
+  const { surah } = context.params;
+  console.log("surah number " + surah);
+  const res = await fetch("http://api.alquran.cloud/v1/surah/" + surah);
+  const { data } = await res.json();
 
   return {
-    props: { surah },
+    props: { data },
   };
 }
